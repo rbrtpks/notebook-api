b77ac4b7a7b900eb5cd64b2f6f886309f388532a
Roberto <rbrtarenhart@gmail.com>
2022-02-19 21:24:53 -0300

    42: Relashionships and Routes (belongs_to)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :kinds
-  resources :contacts
+
+  resources :contacts do
+    resource :kind, only: [:show]
+    resource :kind, only: [:show], path: 'relashionships/kind'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get '/contacts', to: "contacts#index"
 end
